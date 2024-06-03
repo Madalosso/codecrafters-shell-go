ee160488bea26e1dd40da1968d09f10a938491b6
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-06-03 12:35:29 -0300

    support exit code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,18 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	// Wait for user input
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		commandName := input[:len(input)-1]
+		rawInput, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		inputLine := rawInput[:len(rawInput)-1]
+		args := strings.Split(inputLine, "")
 
+		commandName := args[0]
 		switch commandName {
 		case "exit":
-			os.Exit(0)
+			codeStatus, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Invalid exit code:", args[1], err)
+			}
+			os.Exit(codeStatus)
 		default:
 			fmt.Printf("%s: command not found\n", commandName)
 		}
