04303942818ab636846e1f9e31b610c81ae46086
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-06-03 12:31:55 -0300

    add prompt token to the loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,13 +7,9 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
-	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
 	for {
+		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		commandName := input[:len(input)-1]
 		fmt.Printf("%s: command not found\n", commandName)
