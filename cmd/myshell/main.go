5791bdacfc2b6172f1bbded2620bbce46b5f06d4
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-06-03 12:48:10 -0300

    Refactor supported commands into map instead of switch

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,26 +8,34 @@ import (
 	"strings"
 )
 
+var commands = map[string]func(args []string){
+	"exit": func(args []string) {
+		codeStatus, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Invalid exit code:", args[1], err)
+		}
+		os.Exit(codeStatus)
+	},
+	"echo": func(args []string) {
+		fmt.Println(strings.Join(args[1:], " "))
+	},
+}
+
 func main() {
-	// Wait for user input
+
 	for {
+		// Wait for user input
 		fmt.Fprint(os.Stdout, "$ ")
 		rawInput, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		inputLine := rawInput[:len(rawInput)-1]
 		args := strings.Split(inputLine, " ")
 
 		commandName := args[0]
-		switch commandName {
-		case "exit":
-			codeStatus, err := strconv.Atoi(args[1])
-			if err != nil {
-				fmt.Println("Invalid exit code:", args[1], err)
-			}
-			os.Exit(codeStatus)
-		case "echo":
-			fmt.Println(strings.Join(args[1:], " "))
-		default:
+		fn, ok := commands[commandName]
+		if !ok {
 			fmt.Printf("%s: command not found\n", commandName)
+		} else {
+			fn(args)
 		}
 	}
 }
